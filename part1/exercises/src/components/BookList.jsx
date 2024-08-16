9a8ada8faba38554c029c392a800fcca5e573b09
Owner <braydens.coding@gmail.com>
2024-08-16 18:32:06 -0500

    Exercises Part: 1 Complete

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Reading List";
+   let book1 = "https://m.media-amazon.com/images/I/61BvkqoxLxL._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/61NAx5pd6XL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/71O2XIytdqL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Cover art of The Art of War by Sun Tzu" />
+         <img src={book2} alt="Cover art of 1984 by George Orwell" />
+         <img src={book3} alt="Cover art of Crime and Punishment by Fyodor Dostoevsky" />
       </div>      
    );
 }
