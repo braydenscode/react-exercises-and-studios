1660206c3a4f859a031a5d068b23ef57843cf7fe
Owner <braydens.coding@gmail.com>
2024-08-23 19:58:12 -0500

    React Exercise 3 [COMPLETED]

---
+++ part3/exercises/src/components/Introduction.jsx
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction () {
+   return(
+      <div>
+         <h2>3 Fun Facts About Guitar</h2>
+         <ol>
+            <li>The guitar's ancestors date back to over 4,000 years ago, but modern acoustic guitars were only perfected around 1850.</li>
+            <li>The first commercially successful electric guitar was the Fender Broadcaster (now called the Telecaster), introduced in 1950.</li>
+            <li>Guitars come in all sorts of shapes and sizes, and their sounds can also be further affected by different tunings and effects.</li>
+         </ol>
+      </div>
+   );
+}
